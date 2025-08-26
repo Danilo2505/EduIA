b3e08c5a38c0a854d8e8782e6278fb1aed16e488
João Lemes <joaolemes17@hotmail.com>
2025-08-25 20:52:15 -0400

    fix > alterando o nome da rota

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,15 +1,7 @@
 import React from "react";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Stack } from "expo-router";
 
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
-
-export default function TabLayout() {
+export default function TabsLayout() {
   return (
     <Stack
       screenOptions={{
